12100222e45fc48d4d31ff5941b7ff4523e4e162
GREAT JOE COMPUTERS <isaac_onoja@kuantarium>
2024-01-12 16:46:36 +0100

    added splashScreen Circle

--- lib/core/constant/app_asset.dart
+++ lib/core/constant/app_asset.dart
@@ -2,6 +2,7 @@ class AppAssets {
 
   // app asset images here
   static const String logo = 'assets/images/logo.png';
+  static const String slashCircle = 'assets/images/splashCircle.png';
 
   // app asset Icons here
   static const String visibility = 'assets/icons/visibility.svg';
